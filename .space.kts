37d8cb27e4e9b815567ca2802d3be898e0863b86
Alejandro Aviña <alexmark140@gmail.com>
2022-10-06 01:21:07 +0000

    test

--- .space.kts
+++ .space.kts
@@ -1,9 +1,11 @@
-/**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
-
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+/container("openjdk:11") {
+    kotlinScript { api ->
+        api.space().projects.automation.deployments.start(
+            project = api.projectIdentifier(),
+            targetIdentifier = TargetIdentifier.Key("server"),
+            version = "1.0.0",
+            // automatically update deployment status based on a status of a job
+            syncWithAutomationJob = true
+        )
+    }
 }
